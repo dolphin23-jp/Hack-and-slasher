f6902cbbd65f336a5e765ba6c450d21a62dcbb52
dolphin23-jp <h062.dolphin23@gmail.com>
2026-09-21 06:24:26 +0900

    Map smoke clicks to the rendered viewport

--- tests/visual_runner.gd
+++ tests/visual_runner.gd
@@ -165,7 +165,7 @@ func _frames(count: int = 2) -> void:
 		await process_frame
 
 func _click(base_position: Vector2) -> void:
-	var viewport_size := Vector2(root.size)
+	var viewport_size := game.ui.get_viewport_rect().size
 	var screen_position := base_position / BASE * viewport_size
 	var motion := InputEventMouseMotion.new()
 	motion.position = screen_position
@@ -189,7 +189,7 @@ func _click(base_position: Vector2) -> void:
 
 
 func _touch(base_position: Vector2) -> void:
-	var viewport_size := Vector2(root.size)
+	var viewport_size := game.ui.get_viewport_rect().size
 	var screen_position := base_position / BASE * viewport_size
 	var down := InputEventScreenTouch.new()
 	down.index = 0
